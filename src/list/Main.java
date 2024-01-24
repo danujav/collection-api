6d6f02ea80f94b04561ebf80c1cb013ddef9e122
danu <danujagreru@gmail.com>
2024-01-24 15:04:08 +0530

    initial commit

---
+++ src/list/Main.java
@@ -0,0 +1,12 @@
+package list;
+
+/*
+    @author DanujaV
+    @created 1/24/24 - 3:00 PM   
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
