a2b42643745934868803165aff2e54549d65a56a
danu <danujagreru@gmail.com>
2024-01-24 16:19:29 +0530

    rename class name

--- src/set/hashset/ListVsSet.java
+++ src/set/hashset/ListVsSet.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HashSetDemo {
+public class ListVsSet {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(10);
